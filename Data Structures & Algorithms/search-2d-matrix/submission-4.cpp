2c8b4bfd1be864ee27301f26111d879594612c3d
Jonathan-777 <126800463+Jonathan-777@users.noreply.github.com>
2026-05-17 18:54:48 -0500

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        auto search = [&](int pos){
+            int l = 0; 
+            int r = matrix[pos].size()-1;
+
+            while(l <= r){
+                int middle = (l + r) / 2;
+                if(matrix[pos][middle] == target){
+                    return true;
+                }
+                if(matrix[pos][middle] < target){
+                    l = middle + 1;
+                }
+                if( matrix[pos][middle] > target){
+                    r = middle -1;
+                }
+            }
+
+            return (r == 1 &&  matrix[pos][r] == target)? true : false;
+        };
+        
+
+        for(int i = 0 ; i < matrix.size(); ++i){
+            // if(matrix[i].size())
+            int leftNUM = matrix[i][0];
+            int rightNum = matrix[i][matrix[i].size()-1];
+
+            if( leftNUM <= target  && target <= rightNum){
+               return search(i);
+               
+            }
+        }
+
+        
+        return false;
+        
+    }
+};
