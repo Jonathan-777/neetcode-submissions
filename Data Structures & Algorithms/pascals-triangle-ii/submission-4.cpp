4d911d1c0c5d9b7655ee7137b5446db25b84c22a
Jonathan-777 <126800463+Jonathan-777@users.noreply.github.com>
2026-07-01 16:18:52 -0500

    Add: pascals-triangle-ii - submission-4

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-4.cpp
@@ -0,0 +1,16 @@
+
+
+
+
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row = {1};
+        
+        for (int i = 1; i <= rowIndex; i++) {
+            row.push_back(row.back() * 1LL * ((rowIndex - i) + 1) / i  );
+        }
+        return row;
+    }
+};
