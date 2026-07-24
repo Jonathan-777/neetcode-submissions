72e769b26a30b1eac305d14fa8e34c10dc36fe13
Jonathan-777 <126800463+Jonathan-777@users.noreply.github.com>
2026-07-23 23:10:11 -0500

    Add: path-crossing - submission-28

---
+++ Data Structures & Algorithms/path-crossing/submission-28.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    struct Comp{
+        template<typename T1, typename T2>
+        
+        size_t operator()(pair<T1,T2> p)const noexcept(noexcept( hash<int>{}(p.first)) && noexcept( hash<int>{}(p.second))){  //"My custom hashing function promises never to throw an exception if and only if the default hashers for the two pair elements also promise never to throw an exception."
+            size_t h = (hash<T1>()(p.first) << 32) + (hash<T2>()(p.second));
+            return h;
+        }
+    };
+    bool isPathCrossing(string path) {
+
+        int up = 0;
+        int horizontal = 0;
+        unordered_set<pair<int,int>, Comp> s;
+        
+        s.insert({up,horizontal});
+
+        for( const auto& c : path){
+
+            if(c == 'N'){
+                up++;
+            }else if( c == 'S'){
+                up--;
+            }else if( c == 'W'){
+                horizontal++;
+            }else{
+                horizontal--;
+            }
+
+            if(!s.insert({up,horizontal}).second){
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
