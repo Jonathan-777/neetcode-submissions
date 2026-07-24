26174e2cbd048f3e452b4a71acf38406341f8e14
Jonathan-777 <126800463+Jonathan-777@users.noreply.github.com>
2026-07-23 23:05:48 -0500

    Add: path-crossing - submission-25

---
+++ Data Structures & Algorithms/path-crossing/submission-25.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    struct Comp{
+        template<typename T1, typename T2>
+
+        bool operator()(pair<T1,T2> p)const noexcept(noexcept( hash<int>{}(p.first)) && noexcept( hash<int>{}(p.second))){
+            size_t h = (hash<T1>()(p.first) << 32) + (hash<T2>()(p.second));
+            return h;
+        }
+    };
+    bool isPathCrossing(string path) {
+
+        int up = 0;
+        int horizontal = 0;
+        unordered_set<pair<int,int>, Comp> s;
+        s.insert({up,horizontal});
+
+        for( const auto& c : path){
+
+            if(c == 'N'){
+                up++;
+            }else if( c == 'S'){
+                up--;
+            }else if( c == 'W'){
+                horizontal++;
+            }else{
+                horizontal--;
+            }
+
+            if(!s.insert({up,horizontal}).second){
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
